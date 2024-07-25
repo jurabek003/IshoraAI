abe193f3df94d2b775b1b37fdce347b2ea9f01ab
Asus <turgunboyevjurabek777@gmail.com>
2024-07-25 11:13:31 +0500

    asd

--- app/src/main/java/uz/turgunboyevjurabek/ishoraai/MainActivity.kt
+++ app/src/main/java/uz/turgunboyevjurabek/ishoraai/MainActivity.kt
@@ -1,16 +1,26 @@
 package uz.turgunboyevjurabek.ishoraai
 
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
+import android.webkit.ConsoleMessage
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import uz.turgunboyevjurabek.ishoraai.ui.theme.IshoraAITheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +30,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             IshoraAITheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Column(
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {
+                        MyWebView()
+                    }
                 }
             }
         }
@@ -31,17 +42,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyWebView() {
+
+    val mUrl = "http://54.160.153.22:8083/"
+    val  context= LocalContext.current
+    Toast.makeText(context, "VebView start...", Toast.LENGTH_SHORT).show()
+
+    AndroidView(factory = {
+        WebView(it).apply {
+            this.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            this.webChromeClient = CustomWebChromeClient()
+        }
+    }, update = {
+        it.loadUrl(mUrl)
+    })
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IshoraAITheme {
-        Greeting("Android")
+class CustomWebChromeClient : WebChromeClient() {
+    override fun onCloseWindow(window: WebView?) {}
+
+    override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
+        Log.d("jjjj", consoleMessage?.message().toString())
+        return true
     }
 }
+
