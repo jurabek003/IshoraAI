0b95ba17ac065895a906e36d8a465fb8563601c8
Asus <turgunboyevjurabek777@gmail.com>
2024-08-17 15:17:52 +0500

    PagerScreen finished 1.1.0

--- app/src/main/java/uz/turgunboyevjurabek/ishoraai/Presentation/UI_screens/Auth/Page1Screen.kt
+++ app/src/main/java/uz/turgunboyevjurabek/ishoraai/Presentation/UI_screens/Auth/Page1Screen.kt
@@ -3,10 +3,12 @@
 package uz.turgunboyevjurabek.ishoraai.Presentation.UI_screens.Auth
 
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -37,6 +39,7 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -54,6 +57,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
@@ -74,23 +78,29 @@ fun Page1Screen(navController: NavController) {
         topBar = {
             TopAppBar(
                 modifier = Modifier
+                    .height(70.dp)
                     .clip(RoundedCornerShape(bottomEnd = 25.dp, bottomStart = 25.dp)),
                 title = {
-
                 },
                 colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Color.White,
+                    containerColor = if(!isSystemInDarkTheme()){
+                     Color.White
+                    }else{
+                        MaterialTheme.colorScheme.surfaceColorAtElevation(20.dp)
+                    }
                 ),
                 actions = {
-                    IconButton(onClick = { }) {
+                    IconButton(
+                        onClick = { },
+                        modifier = Modifier
+                            .padding(end = 12.dp, bottom = 20.dp)
 
+                    ) {
                         Icon(
                             painter = painterResource(id = R.drawable.ic_x),
-                            contentDescription = "Localized description",
+                            contentDescription = null,
                             tint = colorResource(id = R.color.selected),
                             modifier = Modifier
-                                .size(21.dp)
-                                .align(Alignment.CenterVertically)
                         )
                     }
                 }
@@ -160,6 +170,9 @@ fun Page1Screen(navController: NavController) {
 
                }
             }
+            /**
+             * Button va tab qismi
+             */
             Row(
                 Modifier
                     .wrapContentHeight()
@@ -177,10 +190,10 @@ fun Page1Screen(navController: NavController) {
                             coroutineScope.launch {
                                 pagerState.animateScrollToPage(
                                     count - 1,
-                                    0.3f,
+                                    0.0f,
                                     animationSpec = spring(
                                         dampingRatio = 0.5f,
-                                        stiffness = 200f
+                                        stiffness = 100f
                                     )
                                 )
                             }
@@ -202,14 +215,14 @@ fun Page1Screen(navController: NavController) {
                 repeat(pagerState.pageCount){iteration->
                     val color=if (pagerState.currentPage==iteration) colorResource(id = R.color.selected) else Color.Transparent
                     OutlinedCard(
+                        border = BorderStroke(1.dp, color = colorResource(id = R.color.selected)),
                         colors = CardDefaults.cardColors(
-                            containerColor = color
+                            containerColor = color,
                         ),
                         modifier = Modifier
                             .padding(4.dp)
                             .height(10.dp)
                             .width(26.dp)
-
                     ) {
                     }
                 }
@@ -218,10 +231,10 @@ fun Page1Screen(navController: NavController) {
                         coroutineScope.launch {
                             pagerState.animateScrollToPage(
                                 count + 1,
-                                0.3f,
+                                0.0f,
                                 animationSpec = spring(
                                     dampingRatio = 0.5f,
-                                    stiffness = 200f
+                                    stiffness = 100f
                                 )
                             )
                         }
